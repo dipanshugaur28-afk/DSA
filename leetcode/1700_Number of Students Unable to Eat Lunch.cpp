cc1e87db64e268c86a9c7959a47015ac48449729
dipanshugaur28-afk <dipanshugaur28@gmail.com>
2025-09-10 19:32:56 +0530

    1700_Number of Students Unable to Eat Lunch.cpp

---
+++ leetcode/1700_Number of Students Unable to Eat Lunch.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        queue<int> q;
+        for (int s : students) q.push(s);
+        int i = 0, count = 0;
+        while (!q.empty() && count < q.size()) {
+            if (q.front() == sandwiches[i]) {
+                q.pop();
+                i++;
+                count = 0;
+            } else {
+                q.push(q.front());
+                q.pop();
+                count++;
+            }
+        }
+        return q.size();
+    }
+};
