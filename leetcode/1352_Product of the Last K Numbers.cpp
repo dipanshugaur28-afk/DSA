d3792991f642941b723ef923faee7b84f27f636b
dipanshugaur28-afk <dipanshugaur28@gmail.com>
2025-09-10 19:30:16 +0530

    1352_Product of the Last K Numbers.cpp

---
+++ leetcode/1352_Product of the Last K Numbers.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class ProductOfNumbers {
+    vector<int> prefix;
+public:
+    ProductOfNumbers() {
+        prefix = {1}; 
+    }
+    
+    void add(int num) {
+        if (num == 0) {
+            prefix = {1}; 
+        } else {
+            prefix.push_back(prefix.back() * num);
+        }
+    }
+    
+    int getProduct(int k) {
+        if (k >= prefix.size()) return 0;
+        return prefix.back() / prefix[prefix.size() - k - 1];
+    }
+};
